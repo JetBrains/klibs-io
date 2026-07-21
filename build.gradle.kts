bb0745ec1558d8bbb99106990cc2bb1f6a094187
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2026-07-21 16:49:43 +0200

    Add comment about empty build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,2 @@
+// TODO(KTC-5557): Remove when Dependabot supports dependencies declared in Kotlin Toolchain module.yaml files.
+// This file makes Dependabot detect a Gradle project and update gradle/libs.versions.toml in the meantime.
