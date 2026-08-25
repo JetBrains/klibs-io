a4189d3ac98eb39dfbf52449fbe51ed7794b30dd
Yuri Teplyakov <yuri.teplyakov@jetbrains.com>
2026-08-25 11:51:49 +0200

    ktl 4196 feat: make target popup to show all target groups

--- frontend/src/app/ui/targets-table-popup/index.tsx
+++ frontend/src/app/ui/targets-table-popup/index.tsx
@@ -1,52 +1,10 @@
-import styles from "../targets-table/styles.module.css"
-import cn from "classnames";
-import {textCn} from "@rescui/typography";
-import {PackageOverview, toTargetGroupsByPlatform} from "@/app/types";
+import {PackageOverview} from "@/app/types";
+import TargetsTable from "@/app/ui/targets-table";
 
 interface TargetsTableProps {
     projectPackage: PackageOverview;
 }
 
 export default function TargetsTablePopup({projectPackage} : TargetsTableProps) {
-
-    return (
-        // Wrapper
-        <div className={cn(textCn("rs-text-3", {hardness: "hard"}))} >
-
-            {toTargetGroupsByPlatform(projectPackage.targetGroups, true).map(platform => (
-
-                // Platform wrapper
-                <div key={platform.platformId} className={cn(styles.platformWrapper, styles[platform.platformName.substring(0,2)])}>
-
-                    {/*Group+target wrapper*/}
-                    <div className={styles.groupTargetWrapper}>
-                        {platform.groups.map(group => (
-
-                            // Group wrapper
-                            <div key={group.groupId} className={styles.groupWrapper}>
-
-                                {/*Group label*/}
-                                <div className={cn(textCn("rs-h5"), styles.groupId)}>
-                                    {group.groupId}
-                                </div>
-
-                                 {/*Target wrapper*/}
-                                 <div className={styles.targetWrapper}>
-
-                                    {/* List of all targets*/}
-                                    {group.targets.map(target => (
-                                        <span key={target}>
-                                            {target}
-                                        </span>
-                                    ))}
-
-                                 </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            ))}
-        </div>
-
-    )
+    return <TargetsTable projectPackage={projectPackage}/>
 }
