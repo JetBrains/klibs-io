20c93ce103591f0bc7fbecc3c8c0583266f1f57a
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2026-09-09 11:13:01 +0200

    KTL-2087: Replaced pom.groupId and pom.version to non-null variants
    pom.groupId and pom.version can return null when groupId or version is declared only in the pom.parent. That is why we should check parent's groupId and versions as well.

--- app/src/main/kotlin/io/klibs/app/indexing/PomIndexingService.kt
+++ app/src/main/kotlin/io/klibs/app/indexing/PomIndexingService.kt
@@ -40,7 +40,7 @@ class PomIndexingService(
     }
 
     fun extractDevelopers(pom: MavenPom): List<PackageDeveloper> {
-        val developers = pom.developers ?: return emptyList()
+        val developers = pom.developers
         return developers.mapNotNull { dev ->
             val name = (dev.name ?: dev.organization)
                 ?.takeIf { it.isNotBlank() }
@@ -54,7 +54,7 @@ class PomIndexingService(
     }
 
     fun extractLicenses(pom: MavenPom): List<PackageLicense> {
-        val licenses = pom.licenses ?: return emptyList()
+        val licenses = pom.licenses
         return licenses.mapNotNull { license ->
             val name = license.name?.takeIf { it.isNotBlank() } ?: return@mapNotNull null
             PackageLicense(
@@ -89,7 +89,7 @@ class PomIndexingService(
 
     private fun MavenPom.extractDependencies(): Set<MavenCoordinatesDTO> =
         dependencies
-            ?.asSequence()
+            .asSequence()
             ?.mapNotNull { dep ->
                 val group = dep.groupId?.takeIf { it.isNotBlank() } ?: return@mapNotNull null
                 val artifact = dep.artifactId?.takeIf { it.isNotBlank() } ?: return@mapNotNull null
