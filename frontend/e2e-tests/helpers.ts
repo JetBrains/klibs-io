105d075ea361ae280f3aa18c7598b5beab53ab54
Yuri Teplyakov <yuri.teplyakov@jetbrains.com>
2026-09-11 13:40:59 +0200

    ktl-4936 fix: fix cookie banner selector (#507)

--- frontend/e2e-tests/helpers.ts
+++ frontend/e2e-tests/helpers.ts
@@ -2,6 +2,6 @@ import { Page } from '@playwright/test';
 
 export async function acceptCookiesIfPresent(page: Page) {
     if (process.env.PROD) {
-        await page.locator('button.ch2-btn.ch2-btn-primary').click();
+        await page.locator('.ch2-allow-all-btn').click();
     }
 }
